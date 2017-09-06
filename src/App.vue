35d29fd5eac1b935fc40121763b4c2b3a33f118f
Rafael Vettori <rafael.vettori@gmail.com>
2017-09-06 17:52:52 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <header class="header">
+      <h1>Tarefas</h1>
+      <input-task></input-task>
+    </header>
+  </section>
+</template>
+
+
+<script>
+import InputTask from './components/InputTask'
+
+export default {
+  name: 'app',
+  components: {
+    InputTask
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InputTask.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input class="new-todo" placeholder="O que precisa ser feito?" />
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
